9d1b1d4b8fde806160faf0bb9b12b13f3eb59722
DEVELOPER-BERA <berabruce9@gmail.com>
2025-03-09 10:30:08 +0300

    Add files via upload

---
+++ plugins/ping.js
@@ -0,0 +1,40 @@
+import config from '../config.cjs';
+
+const ping = async (m, Matrix) => {
+  const prefix = config.PREFIX;
+  const cmd = m.body.startsWith(prefix) ? m.body.slice(prefix.length).split(' ')[0].toLowerCase() : '';
+
+  if (cmd === "ping") {
+    const start = new Date().getTime();
+
+    const reactionEmojis = ['🔥', '⚡', '🚀', '💨', '🎯', '🎉', '🌟', '💥', '🕐', '🔹'];
+    const textEmojis = ['💎', '🏆', '⚡️', '🚀', '🎶', '🌠', '🌀', '🔱', '🛡️', '✨'];
+
+    const reactionEmoji = reactionEmojis[Math.floor(Math.random() * reactionEmojis.length)];
+    let textEmoji = textEmojis[Math.floor(Math.random() * textEmojis.length)];
+
+    // Ensure reaction and text emojis are different
+    while (textEmoji === reactionEmoji) {
+      textEmoji = textEmojis[Math.floor(Math.random() * textEmojis.length)];
+    }
+
+    await m.React(textEmoji);
+
+     
+    await client.sendMessage(m.chat, { text: `${botname} speed\n\n *${pingSpeed.toFixed(4)} m/s..*` }, { quoted: customContactMessage });
+
+    
+    await loading(m, client);
+
+  } catch (error) {
+   
+    console.error("Error sending message:", error);
+    m.reply('An error occurred while sending the message.');
+  }
+};
+export default ping;
+
+
+
+ 
+    
