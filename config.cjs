5b37839a034814caebbbdb79e3708d2a373a2cdb
DEVELOPER-BERA <berabruce9@gmail.com>
2025-03-16 13:38:28 +0300

    Update config.cjs

--- config.cjs
+++ config.cjs
@@ -1,43 +1,43 @@
+// config.js
 const fs = require("fs");
 require("dotenv").config();
 
 const config = {
-  SESSION_ID: process.env.SESSION_ID || "Your Session Id",
+  SESSION_ID: process.env.SESSION_ID || "",
   PREFIX: process.env.PREFIX || '.',
-  AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN !== undefined ? process.env.AUTO_STATUS_SEEN === 'true' : true, 
-  AUTO_STATUS_REPLY: process.env.AUTO_STATUS_REPLY !== undefined ? process.env.AUTO_STATUS_REPLY === 'true' : true,
-  STATUS_READ_MSG: process.env.STATUS_READ_MSG || '',
+//let's add menu captions by bera tech
+   BOT_NAME: process.env.BOT_NAME || "Demon Slayer",
+  CAPTION: process.env.CAPTION|| "> *ʙᴇʀᴀ ᴛᴇᴄʜ ʙᴏᴛ*",
+// don't use my codes without a permission 🙏
+  AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN !== undefined ? process.env.AUTO_STATUS_SEEN === 'true' : true,
+  AUTO_BIO: process.env.AUTO_BIO !== undefined ? process.env.AUTO_BIO === 'true' : true,
+  AUTOLIKE_STATUS: process.env.AUTOLIKE_STATUS !== undefined ? process.env.AUTOLIKE_STATUS === 'true' : false,
+  AUTOLIKE_EMOJI: process.env.AUTOLIKE_EMOJI || '🫨', // For liking status updates(stories)
+  AUTO_REPLY_STATUS: process.env.AUTO_REPLY_STATUS !== undefined ? process.env.AUTO_REPLY_STATUS === 'true' : false,
+  STATUS_READ_MSG: process.env.STATUS_READ_MSG || 'Status viewed',
   AUTO_DL: process.env.AUTO_DL !== undefined ? process.env.AUTO_DL === 'true' : false,
   AUTO_READ: process.env.AUTO_READ !== undefined ? process.env.AUTO_READ === 'true' : false,
   AUTO_TYPING: process.env.AUTO_TYPING !== undefined ? process.env.AUTO_TYPING === 'true' : false,
   AUTO_RECORDING: process.env.AUTO_RECORDING !== undefined ? process.env.AUTO_RECORDING === 'true' : false,
   ALWAYS_ONLINE: process.env.ALWAYS_ONLINE !== undefined ? process.env.ALWAYS_ONLINE === 'true' : false,
   AUTO_REACT: process.env.AUTO_REACT !== undefined ? process.env.AUTO_REACT === 'true' : false,
+   /*auto block only for 212 */
   AUTO_BLOCK: process.env.AUTO_BLOCK !== undefined ? process.env.AUTO_BLOCK === 'true' : true,
+  /*joel james tech added anti delete cmd*/
+  ANTI_DELETE: process.env.ANTI_DELETE !== undefined ? process.env.ANTI_DELETE === 'true' : true,
   REJECT_CALL: process.env.REJECT_CALL !== undefined ? process.env.REJECT_CALL === 'true' : false, 
   NOT_ALLOW: process.env.NOT_ALLOW !== undefined ? process.env.NOT_ALLOW === 'true' : true,
-  MODE: process.env.MODE || "public",
-  OWNER_NAME: process.env.OWNER_NAME || "BRUCE BERA",
-  OWNER_NUMBER: process.env.OWNER_NUMBER || "254743982206",
+  MODE: process.env.MODE || "private",
+  OWNER_NAME: process.env.OWNER_NAME || "ʙʀᴜᴄᴇ ʙᴇʀᴀ",
+  OWNER_NUMBER: process.env.OWNER_NUMBER || "218942841878",
+  SUDO_NUMBER: process.env.SUDO_NUMBER || "254740007567",
   GEMINI_KEY: process.env.GEMINI_KEY || "AIzaSyCUPaxfIdZawsKZKqCqJcC-GWiQPCXKTDc",
   WELCOME: process.env.WELCOME !== undefined ? process.env.WELCOME === 'true' : false, 
-
-  // Auto Bio Feature
-  AUTO_BIO: process.env.AUTO_BIO !== undefined ? process.env.AUTO_BIO === 'true' : true,
-  BIO_TEXTS: [
-    "💔 Legends never die | {time}",
-    "🌧️ I still see your shadows in my room | {time}",
-    "🔥 I cannot change you, so I must replace you | {time}",
-    "🌪️ Wishing well of lost souls | {time}",
-    "🌟 pain is part of progress | {time}",
-    "🎤 We ain't making it past 21 | {time}",
-    "💭 Thinking too much, my mind is a maze | {time}",
-    "🚀 Blast off to another galaxy | {time}"
-  ],
+]
 
   // Anti Left Feature
   ANTI_LEFT: process.env.ANTI_LEFT !== undefined ? process.env.ANTI_LEFT === 'true' : true,
-  ANTI_LEFT_MSG: process.env.ANTI_LEFT_MSG || "🚨 You cannot leave this group! You've been added back automatically."
+  ANTI_LEFT_MSG: process.env.ANTI_LEFT_MSG || "*leaving not allowed in this group! You've been added back automatically.*"
 };
 
 module.exports = config;
